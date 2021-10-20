4c1e4a159c52b335b504c2ea272babc45d95efec
Zyad Yasser <zyadyasser6@gmail.com>
2021-10-20 20:06:44 +0200

    Migrate to Firebase and remove local assets

--- script.js
+++ script.js
@@ -1,31 +1,31 @@
 const screens = [
   {
-    canvas: 'images/mac_mockup-a98050a07f.png',
-    video: 'videos/mac.mp4',
+    canvas: 'https://firebasestorage.googleapis.com/v0/b/rave-clone.appspot.com/o/images%2Fmac_mockup-a98050a07f.png?alt=media&token=cec9e2ba-a343-4180-876b-b34fa2ffdba4',
+    video: 'https://firebasestorage.googleapis.com/v0/b/rave-clone.appspot.com/o/videos%2Fmac.mp4?alt=media&token=b483f019-8605-4d9e-9463-c877c5b6e401',
     name: 'Mac',
     icon: 'fa-apple',
     class: 'mac',
 		widthModifier: '78%'
   },
   {
-    canvas: 'images/iphone_mockup-66775c8d53.png',
-    video: 'videos/ios_video.mp4',
+    canvas: 'https://firebasestorage.googleapis.com/v0/b/rave-clone.appspot.com/o/images%2Fiphone_mockup-66775c8d53.png?alt=media&token=1d78a7d3-8960-4d1d-8a9e-6467aa8fa4bf',
+    video: 'https://firebasestorage.googleapis.com/v0/b/rave-clone.appspot.com/o/videos%2Fios_video.mp4?alt=media&token=37f9576e-059f-4410-a6f5-5d91093906bb',
     name: 'iPhone',
     icon: 'fa-apple',
     class: 'iphone',
 		widthModifier: '92%'
   },
   {
-    canvas: 'images/android_mockup-35765a7008.png',
-    video: 'videos/android_video.mp4',
+    canvas: 'https://firebasestorage.googleapis.com/v0/b/rave-clone.appspot.com/o/images%2Fandroid_mockup-35765a7008.png?alt=media&token=373bd08f-018b-43bd-9feb-27655f0886c1',
+    video: 'https://firebasestorage.googleapis.com/v0/b/rave-clone.appspot.com/o/videos%2Fandroid_video.mp4?alt=media&token=7f45bbaa-eb91-47ed-bd13-1c92a5d25c2d',
     name: 'Android',
     icon: 'fa-google-play',
     class: 'android',
 		widthModifier: '93%'
   },
   {
-    canvas: 'images/desktop_mockup-4d79a1ff4b.png',
-    video: 'videos/windows.mp4',
+    canvas: 'https://firebasestorage.googleapis.com/v0/b/rave-clone.appspot.com/o/images%2Fdesktop_mockup-4d79a1ff4b.png?alt=media&token=c69bdf62-dcb6-4e9e-a579-0570890642c6',
+    video: 'https://firebasestorage.googleapis.com/v0/b/rave-clone.appspot.com/o/videos%2Fwindows.mp4?alt=media&token=81882c82-3fe3-4790-95b7-0320a60be725',
     name: 'Windows',
     icon: 'fa-windows',
     class: 'windows',
